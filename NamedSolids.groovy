31ced921d94fe90c33f7c235e092171fbaf25850
Maura Hunter <Maura Hunter@192.168.21.71>
2023-09-12 17:28:09 -0400

    I changed everything to the shapes

--- NamedSolids.groovy
+++ NamedSolids.groovy
@@ -1,7 +1,7 @@
-double size =20
+double size =80
 return [
 new Dodecahedron(size).toCSG().movex(size*0),
-new Icosahedron(size).toCSG().movex(size*2),
+new Icosahedron(size).toCSG().movex(size*10),
 new Octahedron(size).toCSG().movex(size*4),
 new Tetrahedron(size).toCSG().movex(size*6),
 
